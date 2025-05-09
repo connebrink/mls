7cd31846a2928ecefad2d76b47deec450f962609
Clas Onnebrink <clas@gmx.net>
2025-05-09 17:32:37 +0200

    next step

--- src/mls.cpp
+++ src/mls.cpp
@@ -1,6 +1,40 @@
 
-int main(int argc, char *argv[])
+#include <string>
+#include <iostream>
+#include <vector>
+#include <filesystem>
+
+using namespace std;
+
+class MLs
 {
-  
-  return 0;
+private:
+  struct MLSArg {
+    string origArgStr;
+  };
+
+private:
+  vector<MLSArg> arguments;
+
+private:
+  void _readArguments(int argCount, char **argValues) {
+  }
+
+public:
+  MLs(int argCount, char **argValues) {
+    _readArguments(argCount, argValues);
+  }
+
+public:
+  int execute() const {
+    int execResult  {0};
+    return execResult;
+  }
+};
+
+int main(int argCount, char *argValues[])
+{
+  MLs mLs {argCount, argValues};
+  return mLs.execute();
 }
+
